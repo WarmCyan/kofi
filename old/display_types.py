80d065a7e1561f86a5be6145c744243f75463f55
Nathan Martindale <nathanamartindale@gmail.com>
2020-06-17 17:22:40 -0500

    Remove old experiments

--- old/display_types.py
+++
@@ -1,4 +0,0 @@
-from enum import Enum
-
-class DisplayType(Enum):
-    NORMAL = 1
--- old/new.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-filename=$(python -c "import util; print(util.get_uuid())")
-
-#tmux new-session -d -s k
-
-touch cache/$filename
-
-nvim cache/$filename
